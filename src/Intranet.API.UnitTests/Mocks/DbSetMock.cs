56ebd4f65f08ee6c593d5b78572045c71ace388f
muppskaft7413 <mano1321@student.miun.se>
2017-04-19 10:04:09 +0200

    test(api): Adding unit tests for news controller (#33)
    * test(API): Add unit test for news controller get news, work in progress
    
    * test(API): Add test to return all news correctly.
    
    * test(API): Add test for return news by id.
    
    * refactor(api): Update according to change requests

---
+++ src/Intranet.API.UnitTests/Mocks/DbSetMock.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Intranet.API.UnitTests.Mocks
+{
+  public static class DbSetMock
+  {
+    public static Mock<DbSet<TEntity>> MockSet<TEntity>(IEnumerable<TEntity> data)
+        where TEntity : class
+    {
+      var queryableData = data.AsQueryable();
+      var mockSet = new Mock<DbSet<TEntity>>();
+      mockSet.As<IQueryable<TEntity>>().Setup(m => m.Provider).Returns(queryableData.Provider);
+      mockSet.As<IQueryable<TEntity>>().Setup(m => m.Expression).Returns(queryableData.Expression);
+      mockSet.As<IQueryable<TEntity>>().Setup(m => m.ElementType).Returns(queryableData.ElementType);
+      mockSet.As<IQueryable<TEntity>>().Setup(m => m.GetEnumerator()).Returns(() => queryableData.GetEnumerator());
+
+      return mockSet;
+    }
+  }
+}
