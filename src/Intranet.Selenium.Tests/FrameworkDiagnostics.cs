1b7c6cfd41d456102e7c6110b9f49092e5485058
Victor Dalqvist <v.dalqvist@gmail.com>
2017-08-08 16:09:48 +0200

    chore: add test script project.

---
+++ src/Intranet.Selenium.Tests/FrameworkDiagnostics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using OpenQA.Selenium;
+using Intranet.Selenium.Framework;
+using System.Linq;
+
+namespace Intranet.Selenium.Tests
+{
+    public class FrameworkDiagnostics
+    {
+        [Fact]
+        public void ATest()
+        {
+            SeleniumDriver driver = new SeleniumDriver(Browser.Chrome);
+
+            driver.Navigate.GoToUrl("www.google.com");
+
+            driver.Kill();
+        }
+
+        [Fact]
+        public void BTest()
+        {
+            SeleniumDriver driver = new SeleniumDriver(Browser.InternetExplorer);
+
+            driver.Navigate.GoToUrl("www.google.com");
+
+            driver.Kill();
+        }
+    }
+}
