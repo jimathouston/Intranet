312e7c8f81684ee47d4dc661557f88864a20adb1
Victor Dalqvist <v.dalqvist@gmail.com>
2017-08-08 16:51:24 +0200

    feat: add "ScrollToElement" method to SeleniumNavigate

---
+++ src/Intranet.Selenium.Framework/SeleniumInteract.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intranet.Selenium.Framework
+{
+    public class SeleniumInteract
+    {
+        private readonly IWebDriver _driver;
+
+        public SeleniumInteract(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+    }
+}
