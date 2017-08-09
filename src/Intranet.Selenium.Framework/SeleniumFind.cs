c6c8b4a2758ce311d0db9a98563097c95d11f0da
Victor Dalqvist <v.dalqvist@gmail.com>
2017-08-09 16:46:34 +0200

    chore: implement logging for SeleniumFind

--- src/Intranet.Selenium.Framework/SeleniumFind.cs
+++ src/Intranet.Selenium.Framework/SeleniumFind.cs
@@ -9,23 +9,36 @@ namespace Intranet.Selenium.Framework
     public class SeleniumFind
     {
         private readonly IWebDriver _driver;
+        private readonly SeleniumLogger _logger;
 
-        public SeleniumFind(IWebDriver driver)
+        public SeleniumFind(IWebDriver driver, SeleniumLogger logger)
         {
             _driver = driver;
+            _logger = logger;
         }
 
         public Element Elements(By identifier, string elementName)
         {
-            List<IWebElement> elements = new List<IWebElement>(_driver.FindElements(identifier));
-            return new Element(elements, elementName);
+            _logger.Write($"FIND: Element(s) {elementName}");
+            try
+            {
+                List<IWebElement> elements = new List<IWebElement>(_driver.FindElements(identifier));
+                _logger.Write($"{elements.Count} matching Element(s) found");
+                return new Element(elements, elementName);
+            }
+            catch (NoSuchElementException)
+            {
+                _logger.Write("0 matching Element(s) found", Level.Warn);
+                return null;
+            }
         }
 
         public Element Elements(By identifier, string elementName, int timeOut)
         {
             const int pollFreq = 100;
-
             int waited = 0;
+
+            _logger.Write($"FIND: Element(s) {elementName} (timeOut: {timeOut}ms)");
             List<IWebElement> elements = new List<IWebElement>();
 
             while (!ElementExists(identifier, ref elements) && waited <= timeOut)
@@ -36,9 +49,10 @@ namespace Intranet.Selenium.Framework
 
             if (elements.Count > 0)
             {
+                _logger.Write($"{elements.Count} matching Element(s) found");
                 return new Element(elements, elementName);
             }
-            else throw new NoSuchElementException("No matching element found within specified time limit");
+            else _logger.Write("0 matching Element(s) found within time limit", Level.Warn);
         }
 
         private bool ElementExists(By identifier, ref List<IWebElement> elements)
