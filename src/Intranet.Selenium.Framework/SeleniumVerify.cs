beeb191c1cd7971215301b48564841efbde5db81
Victor Dalqvist <v.dalqvist@gmail.com>
2017-08-09 16:30:41 +0200

    chore: add basic constructor to SeleniumVerify

--- src/Intranet.Selenium.Framework/SeleniumVerify.cs
+++ src/Intranet.Selenium.Framework/SeleniumVerify.cs
@@ -7,6 +7,13 @@ namespace Intranet.Selenium.Framework
 {
     public class SeleniumVerify
     {
+        private readonly IWebDriver _driver;
+        private readonly SeleniumLogger _logger;
 
+        public SeleniumVerify(IWebDriver driver, SeleniumLogger logger)
+        {
+            _driver = driver;
+            _logger = logger;
+        }
     }
 }
