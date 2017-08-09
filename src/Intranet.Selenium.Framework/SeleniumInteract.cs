eb4cfbd53b791e6f16a88308002a9066df8fc7e4
Victor Dalqvist <v.dalqvist@gmail.com>
2017-08-09 14:34:09 +0200

    chore: add method scaffolding to SeleniumInteract

--- src/Intranet.Selenium.Framework/SeleniumInteract.cs
+++ src/Intranet.Selenium.Framework/SeleniumInteract.cs
@@ -13,5 +13,30 @@ namespace Intranet.Selenium.Framework
         {
             _driver = driver;
         }
+
+        public void Click(Element element)
+        {
+
+        }
+
+        public void Click(Element element, int xOffset, int yOffset)
+        {
+
+        }
+
+        public void ClickJS(Element element)
+        {
+
+        }
+
+        public void SendKeys(Element element, string input)
+        {
+
+        }
+
+        public void SelectFromDropdown(Element element, string optionToSelect)
+        {
+
+        }
     }
 }
